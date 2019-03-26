60e01bd0410807b7945a528e33d962f7617503e7
Lee Do Kyu <dfdf55dfdf@naver.com>
2019-03-26 13:23:54 +0900

    clean code

--- Poly_C.java
+++ Poly_C.java
@@ -66,18 +66,11 @@ public class Poly_C {
 		System.out.println(total_len);
 		int[] len = {init_len(this), init_len(other)};
 		int i=0, j=0, k=0;
-		//System.out.println("#####################");
-		//System.out.println(len);
 		Poly_C temp_poly = new Poly_C(total_len);
 		
 		temp_poly.init_poly(temp_poly, temp_poly.terms.length);
 		System.out.println(len[1]);
 		while(i<len[0] && j<len[1]){
-			//System.out.println("#####################");
-			//System.out.println(this.terms[i].exp);
-			//System.out.println(other.terms[j].exp);
-			//System.out.println("#####################");
-
 			if(this.terms[i].exp == other.terms[j].exp){
 				temp_poly.terms[k].coef = this.terms[i].coef + other.terms[j].coef;
 				temp_poly.terms[k].exp = this.terms[i].exp;
@@ -115,9 +108,6 @@ public class Poly_C {
 			}
 		}
 		else{
-			//System.out.println(i);
-			//System.out.println(j);
-			//System.out.println(k);
 			while(k<total_len){
 				temp_poly.terms[k].coef = this.terms[i].coef;
 				temp_poly.terms[k].exp = this.terms[i].exp;
@@ -130,12 +120,10 @@ public class Poly_C {
 		
 		return temp_poly;		      
         }
-	//public ArrayCount(){
 	
 	public int get_len(Poly_C poly1, Poly_C poly2){
 		int long_len = 0;
 		int[] len = {init_len(poly1), init_len(poly2) };
-		//int len2 = ;
 		int total_len = len[0]+len[1];
 		int copy_len = total_len;
 
@@ -275,23 +263,11 @@ public class Poly_C {
 	else{
 	    temp_poly.next = exp_count;
 	}
-	//for(int i=0; i<max_len; i++){
-	//	if(temp_exp
-
-	//System.out.println(Arrays.toString(temp_exp));
-	//System.out.println(Arrays.toString(temp_count));
-
-	//System.out.println(Arrays.toString(temp_poly.));
-	//System.out.println(temp_poly.toString());
-
 	return temp_poly;
 		
-	
-        // you code goes here
     }
 
     public String toString() {
-        // Sample code ... you can freely modify this code if necessary
         Arrays.sort(terms, 0, next, (a, b) -> b.exp - a.exp);
         return Arrays.stream(terms)
                      .filter(i -> i != null)
@@ -302,102 +278,28 @@ public class Poly_C {
 		
 
 	public static void main(String... args){
-	        //Poly_C poly = new Poly_C(3);
-        	//poly.addTerm(1, 0);
-        	//poly.addTerm(2, 1);
-        	//poly.addTerm(3, 2);
-		//System.out.println(poly.toString());
-        	//assertEquals("3x^2 + 2x^1 + 1x^0", poly.toString());
-        	//assertEquals(2, poly.degree());
-        	//assertEquals(3, poly.getTermCount());	
-        	//Poly_C poly = new Poly_C(1);
-	        //poly.addTerm(1, 0);
-
-	        //assertEquals("1x^0", poly.toString());
-		//assertEquals(0, poly.degree());
-	        //assertEquals(1, poly.getTermCount());
-		//System.out.println(poly.toString());
-		//System.out.println(poly.degree());
-		//System.out.println(poly.getTermCount());
-        	
-
-        	//poly = new Poly_C(1);
-	        //poly.addTerm(2, 1);
-
-	        //assertEquals("2x^1", poly.toString());
-	        //assertEquals(1, poly.degree());
-	        //assertEquals(1, poly.getTermCount());
-		//System.out.println(poly.toString());
-		//System.out.println(poly.degree());
-		//System.out.println(poly.getTermCount());		
-		/*
-		Poly_C poly1 = new Poly_C(4);
-		poly1.addTerm(2,1);
-	        poly1.addTerm(1,0);
-	        poly1.addTerm(3,2);
-	        poly1.addTerm(4,3);
-	        System.out.println(poly1.toString());
-		System.out.println(poly1.degree());
-		System.out.println(poly1.getTermCount());
-		
-		Poly_C poly = new Poly_C(3);
-      		poly.addTerm(3, 2);
-        	poly.addTerm(2, 1);
-        	poly.addTerm(1, 0);
-
-        	Poly_C unitPoly = new Poly_C(0);
-		System.out.println(poly.add(unitPoly).toString());
-		*/
 
 	        Poly_C poly1 = new Poly_C(4);
-	        //poly1.addTerm(1, 3);
+	        poly1.addTerm(1, 3);
 	        poly1.addTerm(1, 2);
 	        poly1.addTerm(1, 1);
 	        poly1.addTerm(1, 0);
-		//System.out.println(poly1.getTermCount());
+
 	        Poly_C poly2 = new Poly_C(4);
       		poly2.addTerm(1, 1);
         	poly2.addTerm(1, 2);
-        	//poly2.addTerm(1, 3);
-        	//poly2.addTerm(1, 0);
-		//System.out.println(poly2.getTermCount());
+        	poly2.addTerm(1, 3);
+        	poly2.addTerm(1, 0);
+
 		System.out.println(poly1);
 		System.out.println(poly2);
+
 	        Poly_C poly3 = poly1.add(poly2);
 		System.out.println(poly3.toString());		
-		//System.out.println(poly3.toString());
-		System.out.println(poly3.getTermCount());
-		System.out.println(poly3.degree());
+
 		Poly_C poly4 = poly1.mult(poly2);
 		System.out.println(poly4.toString());
-		//System.out.println(poly4.getTermCount());	
-		//System.out.println(poly4.degree());	
-		//Poly_C poly2 = new Poly_C(3);
-		//poly1.addTerm(1,3);
-	        //poly2.addTerm(2,2);
-	        //poly2.addTerm(3,1);
-	        //poly2.addTerm(4,0);
-	        //System.out.println(poly2);
-		//System.out.println(poly2.degree());
-		//System.out.println(poly2.getTermCount());
-		
-		//Poly_C[] Choice_Poly = {poly1, poly2};
-		
-		//System.out.println(Choice_Poly[0]);
-		//System.out.println(Choice_Poly[1]);
-		
-        	//Poly_C poly3 = poly1.add(poly2);
-        	//System.out.println(poly3);		
-                //ArrayTest b = a;
 
-		//b.terms[0].coef = 1;
-		//a.terms[0].coef = 1;
-		//a.terms[1].coef = 2;
-		//System.out.println(a.terms.length);
-		//System.out.println(b.terms.length);
-		//System.out.println(a.terms[0].coef);
-		//System.out.println(a.degree());
-		//System.out.println(b.terms[0].coef == a.terms[0].coef);
 	}
 }
 
